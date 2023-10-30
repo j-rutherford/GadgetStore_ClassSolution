055d654c0e103c84cb53f3553f3c52dcee9dcf4a
Jeremy Rutherford <jrutherford@centriq.com>
2023-10-30 14:01:02 -0500

    converted a template and imported some Notes views and controller.

---
+++ GadgetStore.UI.MVC/Controllers/NotesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GadgetStore.UI.MVC.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult EntityFramework()
+        {
+            return View();
+        }
+
+        public IActionResult Identity()
+        {
+            return View();
+        }
+
+        public IActionResult Metadata()
+        {
+            return View();
+        }
+
+        //public IActionResult Scaffolding()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Security()
+        {
+            return View();
+        }
+
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+
+        public IActionResult ViewCleanup()
+        {
+            return View();
+        }
+    }
+}
